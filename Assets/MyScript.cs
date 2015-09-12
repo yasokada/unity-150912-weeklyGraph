992a63dd7da41cb3316e132609c1148b569a3eb7
y_okada <yasokada@gmail.com>
2015-09-12 18:39:27 +0900

    get days from sunday

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -2,6 +2,11 @@
 using System.Collections;
 using System; // for TimeSpan
 
+/*
+ * v0.1 2015/09/12
+ *   - get days from sunday
+ */
+
 public class MyScript : MonoBehaviour {
 
 	System.DateTime getSundayH00M00S00(System.DateTime dt) {
@@ -11,13 +16,14 @@ public class MyScript : MonoBehaviour {
 	}
 
 	void Test_weekly() {
-		Debug.Log ("test weekly");
-
 		System.DateTime now = System.DateTime.Now;
 		System.DateTime sunday = getSundayH00M00S00 (now);
 
-		Debug.Log (now.ToString ());
-		Debug.Log (sunday.ToString ());
+		int daysdiff = now.Subtract (sunday).Days;
+
+		Debug.Log ("now: " + now.ToString ());
+		Debug.Log ("sunday: " + sunday.ToString ());
+		Debug.Log ("days: " + daysdiff);
 	}
 
 	void Start () {
