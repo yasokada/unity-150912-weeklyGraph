92a9e7895e4b3554e391c5e6ba59bcebc92ea592
y_okada <yasokada@gmail.com>
2015-09-12 19:03:11 +0900

    add getDayHourMinFractionOfWeek()

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System; // for TimeSpan
 
 /*
+ * v0.2 2015/09/12
+ *   - add getDayHourMinFractionOfWeek()
  * v0.1 2015/09/12
  *   - get days from sunday
  */
@@ -15,15 +17,28 @@ public class MyScript : MonoBehaviour {
 		return dt;
 	}
 
-	void Test_weekly() {
-		System.DateTime now = System.DateTime.Now;
-		System.DateTime sunday = getSundayH00M00S00 (now);
+	float getDayHourMinFractionOfWeek(System.DateTime dt) {
+		System.DateTime sunday = getSundayH00M00S00 (dt);
+		
+		int daysdiff = dt.Subtract (sunday).Days;
+		
+		int hourMin_min = dt.Hour * 60 + dt.Minute;
+		float hourMinFraction = (float)hourMin_min / (24f * 60f); // 24 hours x 60 minutes
+		float ddhhmmFraction = (float)daysdiff + hourMinFraction;
 
-		int daysdiff = now.Subtract (sunday).Days;
-
-		Debug.Log ("now: " + now.ToString ());
+		Debug.Log ("target: " + dt.ToString ());
 		Debug.Log ("sunday: " + sunday.ToString ());
 		Debug.Log ("days: " + daysdiff);
+		Debug.Log ("ddhhmmFraction: " + ddhhmmFraction);
+
+		return ddhhmmFraction / 7.0f; // 7 days a week
+	}
+
+
+	void Test_weekly() {
+		float daysFraction;
+		daysFraction= getDayHourMinFractionOfWeek (System.DateTime.Now);
+		Debug.Log (daysFraction.ToString ("0.000"));
 	}
 
 	void Start () {
